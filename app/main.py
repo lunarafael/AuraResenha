db6404bc50a9b9984427a65295f09f53cb0632e9
Rafael Luna <82469173+lunarafael@users.noreply.github.com>
2025-10-23 16:06:10 -0300

    Feature/post (#2)
    * Implement CRUD operations for posts with authentication and database integration
    
    * Rename read_users_me function to read_current_user to remove warning
    
    * Fixed relationship typo

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.router import api_router
+from app.api.routes import auth, post
 from app.models.base_class import Base
 from app.db.session import engine
 from app.db import base
@@ -9,6 +10,9 @@ app = FastAPI(title="AuraResenha API")
 Base.metadata.create_all(bind=engine)
 
 app.include_router(api_router, prefix="/api/v1")
+app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
+app.include_router(post.router, prefix="/api/v1/posts", tags=["posts"])
+
 
 @app.get("/")
 def root():
---
+++ app/schemas/post.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+
+class PostCreate(PostBase):
+    pass
+
+class PostUpdate(PostBase):
+    pass
+
+class PostOut(PostBase):
+    id: int
+    author_id: int
+    created_at: datetime
+    updated_at: datetime | None = None
+
+    class Config:
+        from_attributes = True
